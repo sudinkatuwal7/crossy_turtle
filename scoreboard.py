383800faf5206e451a17b13843a4d9bff153095e
Sudin Katuwal <sudcrack2024@gmail.com>
2026-01-28 00:58:07 -0600

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,7 +1,7 @@
 from turtle import Turtle
 
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 20, "normal")
 
 
 class Scoreboard(Turtle):
@@ -15,11 +15,16 @@ class Scoreboard(Turtle):
 
     def update_score(self):
         self.clear()
-        self.goto(-280, 280)
+        self.goto(-200, 240)
         self.write(f"Level: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
         self.update_score()
 
+    def game_over(self):
+        self.goto(0,0)
+        self.write("Abui khelna aayena bhai lai", align=ALIGNMENT, font=FONT)
+        self.goto(0, -40)
+        self.write(f"Ule le le le", align=ALIGNMENT, font=FONT)
 
